eb9f027c5afbadd83a9365f1b6b5c70713820996
Adhitia Nugraha <adhitia.nugrahatkj@gmail.com>
2023-01-04 22:14:20 +0700

    fixed bug where the months didnt update with new year

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,23 @@
-from genericpath import isfile
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
-from .secret import SECRET_KEY
 from flask_sslify import SSLify
+from .secret import SECRET_KEY
+from genericpath import isfile
+
 import os
 
 
 db = SQLAlchemy()
-DB_NAME = 'app.db'
+DB_NAME = "app.db"
 
 
 def start_app():
     app = Flask(__name__)
-    app.config['DEBUG'] = False # TODO change this before production
-    app.config['SECRET_KEY'] = SECRET_KEY
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-
+    app.config["DEBUG"] = True  # TODO change this before production
+    app.config["SECRET_KEY"] = SECRET_KEY
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
 
     csrf = CSRFProtect(app)
 
@@ -31,27 +31,26 @@ def start_app():
     create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_staff(id):
         return Staff.query.get(int(id))
 
-    app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(operation, url_prefix='/')
+    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(operation, url_prefix="/")
 
-
-    if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
+    if "DYNO" in os.environ:  # only trigger SSLify if the app is running on Heroku
         sslify = SSLify(app)
 
     return app
 
 
 def create_database(app):
-    if isfile('app/'+DB_NAME):
-        print('--- database already created!')
+    if isfile("app/" + DB_NAME):
+        print("--- database already created!")
         return
     db.create_all(app=app)
-    print('### database successfully created!')
+    print("### database successfully created!")
--- app/operation.py
+++ app/operation.py
@@ -10,118 +10,168 @@ from .models import Year, Month, Client, PaymentData, Staff
 from .module import format_pdf_title
 
 
-operation = Blueprint('operation', __name__)
+operation = Blueprint("operation", __name__)
 
 
 # konfigurasi wkhtmltopdf untuk deployment di heroku
 # NOTE silahkan ubah sesuai kebutuhan server deployment
-WKHTMLTOPDF_CMD = ''
-if platform.system() == 'Windows':
-    WKHTMLTOPDF_CMD = 'app/bin/windows/wkhtmltopdf.exe'
+WKHTMLTOPDF_CMD = ""
+if platform.system() == "Windows":
+    WKHTMLTOPDF_CMD = "app/bin/windows/wkhtmltopdf.exe"
 else:
-    WKHTMLTOPDF_CMD = '/usr/bin/wkhtmltopdf'
+    WKHTMLTOPDF_CMD = "/usr/bin/wkhtmltopdf"
 pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
 
 # inisialisasi dan update database secara otomatis ke tahun sekarang sebelum mengakses daftar client
-@operation.route('/init/<int:from_login>')
+@operation.route("/init/<int:from_login>")
 @login_required
 def init(from_login):
     year = Year.query.filter_by(year_name=str(date.today().year)).first()
-    month_preset = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
-                    'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+    month_preset = [
+        "Januari",
+        "Februari",
+        "Maret",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Agustus",
+        "September",
+        "Oktober",
+        "November",
+        "Desember",
+    ]
     if not year:
-        print('generated year')
+        print("generated year")
         new_year = Year(year_name=str(date.today().year))
         db.session.add(new_year)
         db.session.commit()
         if not from_login:
-            return redirect(url_for('operation.init'))
+            return redirect(url_for("operation.init"))
         else:
-            return redirect(url_for('operation.init', from_login=from_login))
+            return redirect(url_for("operation.init", from_login=from_login))
 
-    month = Month.query.all()
+    month = Month.query.filter_by(year_id=year.id).first()
     if not month:
-        print('generated month')
+        print("generated month")
         for num in range(0, 12):
-            new_month = Month(
-                month_name = month_preset[num],
-                year_id = year.id
-            )
+            new_month = Month(month_name=month_preset[num], year_id=year.id)
             db.session.add(new_month)
             db.session.commit()
 
+    clients = Client.query.all()
+    month_in_year = year.months
+
+    for client in clients:
+        for month in month_in_year:
+            cek_payment_data_month = PaymentData.query.filter_by(
+                payer=client.id, paid_month=month.id
+            ).first()
+            if cek_payment_data_month:
+                continue
+            new_payment_data = PaymentData(
+                paid_month=month.id,
+                paid_year=year.id,
+                payer=client.id,
+                isPaid=False,
+                date_paid=None,
+                last_edit_by=current_user.id,
+                last_edit_date=date.today(),
+            )
+            db.session.add(new_payment_data)
+            db.session.commit()
+
     if not from_login:
-        return redirect(url_for('views.client_list'))
+        return redirect(url_for("views.client_list"))
     else:
-        return redirect(url_for('views.dashboard'))
+        return redirect(url_for("views.dashboard"))
 
 
 # endpoint fungsi untuk meng-archive client
-@operation.route('/archive/<int:client_id>')
+@operation.route("/archive/<int:client_id>")
 @login_required
 def archive_client(client_id):
     client = Client.query.filter_by(id=client_id).first()
 
-    to_archive = Client.query.filter_by(id=client_id).update({
-        Client.is_archived : True
-    })
+    to_archive = Client.query.filter_by(id=client_id).update({Client.is_archived: True})
     db.session.commit()
 
-    flash(f'Berhasil memasukan {client.first_name} {client.last_name} {client.call_name} archive', category='success')
-    return redirect(url_for('views.client_list', current_staff=current_user))
+    flash(
+        f"Berhasil memasukan {client.first_name} {client.last_name} {client.call_name} archive",
+        category="success",
+    )
+    return redirect(url_for("views.client_list", current_staff=current_user))
 
 
 # endpoint fungsi untuk mengembalikan client dari archive ke daftar client
-@operation.route('/unarchive/<int:client_id>')
+@operation.route("/unarchive/<int:client_id>")
 @login_required
 def unarchive_client(client_id):
     client = Client.query.filter_by(id=client_id).first()
 
-    to_archive = Client.query.filter_by(id=client_id).update({
-        Client.is_archived : False
-    })
+    to_archive = Client.query.filter_by(id=client_id).update(
+        {Client.is_archived: False}
+    )
     db.session.commit()
 
-    flash(f'Berhasil mengeluarkan {client.call_name} dari archive', category='success')
-    return redirect(url_for('views.archive_list', current_staff=current_user))
+    flash(f"Berhasil mengeluarkan {client.call_name} dari archive", category="success")
+    return redirect(url_for("views.archive_list", current_staff=current_user))
 
 
 # cetak kartu pdf
-@operation.route('cetakkartu/pdf/<int:client_id>/<string:year>')
+@operation.route("cetakkartu/pdf/<int:client_id>/<string:year>")
 @login_required
 def cetak_kartu(client_id, year):
     #     # query data inisial untuk pemuatan halaman
     client = Client.query.filter_by(id=client_id).first()
     client = Client.query.filter_by(id=client_id).first()
     year_object = Year.query.filter_by(year_name=year).first()
-    payment_data = PaymentData.query.filter_by(payer=client.id, paid_year=year_object.id).all()
+    payment_data = PaymentData.query.filter_by(
+        payer=client.id, paid_year=year_object.id
+    ).all()
     year_list = Year.query.all()
     staff_list = Staff.query.all()
-    month_in_year = year_object.months
+    month_in_year = Month.query.filter_by(year_id=year_object.id).all()
     monthly_payment_date = []
 
     # populasi list monthly_payment_data dengan data tanggal pembayaran
     for num in range(12):
-        the_month = PaymentData.query.filter_by(payer=client.id, paid_year=year_object.id, paid_month=num+1).first()
+        the_month = PaymentData.query.filter_by(
+            payer=client.id, paid_year=year_object.id, paid_month=month_in_year[num].id
+        ).first()
         monthly_payment_date.append(the_month.date_paid)
 
-    rendered = render_template('cetakkartu.html', client=client, year=year, year_list=year_list, months=month_in_year,
-            month_payment_data=zip(payment_data, month_in_year, monthly_payment_date), staff_list=staff_list)
+    rendered = render_template(
+        "cetakkartu.html",
+        client=client,
+        year=year,
+        year_list=year_list,
+        months=month_in_year,
+        month_payment_data=zip(payment_data, month_in_year, monthly_payment_date),
+        staff_list=staff_list,
+    )
 
     # NOTE silahkan ubah sesuai kebutuhan server deployment
-    pdf = pdfkit.from_string(rendered, False, configuration=pdfkit_config, css='/home/sistempembayaranbm/PortalPembayaranBM_v2/app/static/css/cetakkartu.css')
+    pdf = pdfkit.from_string(
+        rendered,
+        False,
+        configuration=pdfkit_config,
+        css="/home/sistempembayaranbm/PortalPembayaranBM_v2/app/static/css/cetakkartu.css",
+    )
 
-    pdf_title = format_pdf_title(f'{client.first_name}_{client.last_name}_({client.call_name})_{year}')
+    pdf_title = format_pdf_title(
+        f"{client.first_name}_{client.last_name}_({client.call_name})_{year}"
+    )
     response = make_response(pdf)
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = f'inline; filename={pdf_title}.pdf'
+    response.headers["Content-Type"] = "application/pdf"
+    response.headers["Content-Disposition"] = f"inline; filename={pdf_title}.pdf"
 
     return response
 
 
 # cetak kuitansi pdf
-@operation.route('cetakkuitansi/pdf/<int:client_id>/<string:year>/<int:kuitansi_id>')
+@operation.route("cetakkuitansi/pdf/<int:client_id>/<string:year>/<int:kuitansi_id>")
 @login_required
 def cetak_kuitansi(client_id, year, kuitansi_id):
     # query data inisial untuk pemuatan halaman
@@ -129,32 +179,53 @@ def cetak_kuitansi(client_id, year, kuitansi_id):
     kuitansi = PaymentData.query.filter_by(id=kuitansi_id).first()
     client = Client.query.filter_by(id=client_id).first()
     year_object = Year.query.filter_by(year_name=year).first()
-    payment_data = PaymentData.query.filter_by(payer=client.id, paid_year=year_object.id).all()
+    payment_data = PaymentData.query.filter_by(
+        payer=client.id, paid_year=year_object.id
+    ).all()
     year_list = Year.query.all()
     staff_list = Staff.query.all()
-    month_in_year = year_object.months
+    month_in_year = Month.query.filter_by(year_id=year_object.id).all()
     monthly_payment_date = []
     kuitansi = PaymentData.query.filter_by(id=kuitansi_id).first()
 
     # populasi list monthly_payment_data dengan data tanggal pembayaran
     for num in range(12):
-        the_month = PaymentData.query.filter_by(payer=client.id, paid_year=year_object.id, paid_month=num+1).first()
+        the_month = PaymentData.query.filter_by(
+            payer=client.id, paid_year=year_object.id, paid_month=month_in_year[num].id
+        ).first()
         monthly_payment_date.append(the_month.date_paid)
 
     if not kuitansi.isPaid:
-        flash('Pembayaran belum lunas.', category='error')
-        return redirect(url_for('views.display_data_pembayaran', client_id=client_id, year=year))
+        flash("Pembayaran belum lunas.", category="error")
+        return redirect(
+            url_for("views.display_data_pembayaran", client_id=client_id, year=year)
+        )
 
-    rendered = render_template('cetakkuitansi.html', client=client, year=year, year_list=year_list, kuitansi=kuitansi, months=month_in_year,
-            month_payment_data=zip(payment_data, month_in_year, monthly_payment_date), staff_list=staff_list)
+    rendered = render_template(
+        "cetakkuitansi.html",
+        client=client,
+        year=year,
+        year_list=year_list,
+        kuitansi=kuitansi,
+        months=month_in_year,
+        month_payment_data=zip(payment_data, month_in_year, monthly_payment_date),
+        staff_list=staff_list,
+    )
 
     # NOTE silahkan ubah sesuai kebutuhan server deployment
-    pdf = pdfkit.from_string(rendered, False, configuration=pdfkit_config, css='/home/sistempembayaranbm/PortalPembayaranBM_v2/app/static/css/cetakkuitansi.css')
+    pdf = pdfkit.from_string(
+        rendered,
+        False,
+        configuration=pdfkit_config,
+        css="/home/sistempembayaranbm/PortalPembayaranBM_v2/app/static/css/cetakkuitansi.css",
+    )
 
-    pdf_title = format_pdf_title(f'{client.first_name}_{client.last_name}_({client.call_name})_{year}')
+    pdf_title = format_pdf_title(
+        f"{client.first_name}_{client.last_name}_({client.call_name})_{year}"
+    )
     print(pdf_title)
     response = make_response(pdf)
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = f'inline; filename={pdf_title}.pdf'
+    response.headers["Content-Type"] = "application/pdf"
+    response.headers["Content-Disposition"] = f"inline; filename={pdf_title}.pdf"
 
     return response
